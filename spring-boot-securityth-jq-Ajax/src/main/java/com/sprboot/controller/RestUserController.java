5c0813b684db98df9ceb0ffe2ec2df5352d78423
Alexandr Orishech <ru0160@icloud.com>
2019-11-28 22:57:08 +0300

    add dynamic select, role user reqest with object user

--- spring-boot-securityth-jq-Ajax/src/main/java/com/sprboot/controller/RestUserController.java
+++ spring-boot-securityth-jq-Ajax/src/main/java/com/sprboot/controller/RestUserController.java
@@ -2,8 +2,10 @@ package com.sprboot.controller;
 
 import com.sprboot.model.Role;
 import com.sprboot.model.User;
+import com.sprboot.service.RoleService;
 import com.sprboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -11,8 +13,16 @@ import java.util.List;
 
 @RestController
 public class RestUserController {
+
     @Autowired
     private UserService userService;
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping("/admin/allRoles")
+    public List<Role> roles(){
+        return roleService.getRoles();
+    }
 
     @GetMapping("/admin/users")
     public List<User> users() {
@@ -25,20 +35,28 @@ public class RestUserController {
     }
 
     @PostMapping("/admin/createuser/")
-    public void addUser(@ModelAttribute User user, @RequestParam String role){
+    public void addUser(@RequestBody User user){
+        String role = new String();
+        for(Role roleInp : user.getRoles()){
+            role = roleInp.getName();
+        }
         userService.updateUser(user,role);
     }
 
     @PutMapping("/admin/edit/")
-    public void editUser(@RequestBody User user, @RequestParam String role) {
+    public void editUser(@RequestBody User user) {
         User userEdit = userService.getUser(user.getId());
+        String role = new String();
+        for(Role roleInp : user.getRoles()){
+            role = roleInp.getName();
+        }
         userEdit.setName(user.getName());
         user.setPassword(user.getPassword());
         userService.updateUser(user,role);
     }
 
-    @DeleteMapping("/admin/delete/{id}")
-    public void deleteUser(@PathVariable Integer id){
+    @DeleteMapping("/admin/delete/")
+    public void deleteUser(@RequestParam  int id){
         userService.deleteUser(id);
     }
 }
--- spring-boot-securityth-jq-Ajax/src/main/resources/static/js/admin.js
+++ spring-boot-securityth-jq-Ajax/src/main/resources/static/js/admin.js
@@ -1,6 +1,25 @@
 
 $(document).ready(function()
 {
+
+    $.getJSON("/admin/allRoles", function (data) {
+        var select = document.querySelector('select');
+        var allRolles = new Array();
+        $.each(data, function (key,value) {
+            allRolles[key] = value.name;
+        })
+        var myDiv = document.getElementById("selectAddUser");
+
+        var selectList = document.createElement("select");
+        selectList.setAttribute("id", "mySelect");
+        myDiv.appendChild(selectList);
+        for (var i = 0; i < allRolles.length; i++) {
+            var option = document.createElement("option");
+            option.setAttribute("value", allRolles[i]);
+            option.text = allRolles[i];
+            selectList.appendChild(option);
+        }
+    });
     $.ajax({
         url: "/admin/users",
         type: "GET",
@@ -20,6 +39,7 @@ $(document).ready(function()
                             <th>" + userRole + "</th> \
                             <th> <button onclick='edt("+ value.id +")' type=\"button\" class=\"btn btn-primary\" \
                             data-toggle=\"modal\" data-target=\"#edituser\" id='usrBtn' >Edit</button> </th> \
+                            <th> <button onclick='del("+ value.id +")' class=\"btn btn-danger\">Delete</button> </th> \
                     </tr>"
                 );
             });
@@ -30,30 +50,61 @@ $(document).ready(function()
         editUser();
     })
 
-    $("#submitEdit").on('click', function () {
+    $("#AddUserBtn").on('click', function () {
         addUser();
     })
 
 });
 
 function edt(id) {
-    $.getJSON("/admin/getUserById/" + id, function(data) {
-        $("#disabledTextInput").val(data.id);
-        $("#nameUser").val(data.name);
-        $("#password").val(data.password);
-        var role = "";
-        $.each(data.roles, function (key,value){
-            $("#role").val(value.name);
+    var select = document.querySelector('select');
+    $.getJSON("/admin/allRoles", function (data) {
+        var allRolles = new Array();
+        $.each(data, function (key,value) {
+            allRolles[key] = value.name;
+        })
+        $.getJSON("/admin/getUserById/" + id, function(data) {
+            $("#disabledTextInput").val(data.id);
+            $("#nameUser").val(data.name);
+            $("#password").val(data.password);
+            var role = "";
+            $.each(data.roles, function (key,value){
+                role = value.name;
+            });
+            for(var i = 0; i < allRolles.length; i++ ){
+                select.add(new Option(allRolles[i]));
+                if(select.options[i].value == role){
+                    select[i].selected = true;
+                }
+            }
         });
     });
-
-
 }
+
+function del(id) {
+    $.ajax({
+        type : "DELETE",
+        url : "/admin/delete/?id=" + id,
+        success: function(data){
+            $(location).attr('href',"/admin/listUsers");
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+}
+
 function editUser(){
+    var selind = document.getElementById("userRole").options.selectedIndex;
+    var val= document.getElementById("userRole").options[selind].value;
+    var roles = [{
+        "name" : val,
+    }]
     var user = {
         "id" : $('#disabledTextInput').val(),
         "name" : $('#nameUser').val(),
         "password" : $('#password').val(),
+        "roles" : roles
 
     }
 
@@ -63,7 +114,7 @@ function editUser(){
             'Content-Type': 'application/json'
         },
         type: "PUT",
-        url: "/admin/edit/?role=" + $('#role').val(),
+        url: "/admin/edit/",
         datatype: 'application/json',
         data: JSON.stringify(user),
         success: function(data){
@@ -76,11 +127,17 @@ function editUser(){
 }
 
 function addUser() {
+    var selind = document.getElementById("mySelect").options.selectedIndex;
+    var val= document.getElementById("mySelect").options[selind].value;
+    var role = [{
+        "name" : val,
+    }]
     var user = {
         "name" : $('#userName').val(),
         "password" : $('#userPassword').val(),
-
+        "roles" : role,
     }
+
 
     $.ajax({
         headers: {
@@ -88,7 +145,7 @@ function addUser() {
             'Content-Type': 'application/json'
         },
         type: "POST",
-        url: "/admin/createuser/?role=" + $('#role').val(),
+        url: "/admin/createuser/",
         datatype: 'application/json',
         data: JSON.stringify(user),
         success: function(data){
@@ -98,7 +155,16 @@ function addUser() {
             alert(error);
         }
     }).done()
+}
 
+function selectAllRoles() {
+    $.getJSON("/admin/allRoles", function (data) {
+        var allRolles = new Array();
+        $.each(data, function (key,value) {
+            allRolles[key] = value.name;
+        })
+        return allRolles;
+    })
 }
 
 
